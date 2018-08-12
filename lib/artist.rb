70b6eaf0186cd0a9a307dcda69228ded1bbbc3ad
Kevin K <doilerjar@outlook.com>
2018-08-12 04:32:32 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+  @@all = []
+  
+  attr_accessor :name, :songs, :genres
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @genres = []
+    
+    @@all << self
+  end
+  
+  def new_song(song_name, genre)
+    song = Song.new(song_name, self, genre)
+    @genres << genre
+    @songs << song
+  end 
+  
+  def self.all
+    @@all
+  end
+
+end
