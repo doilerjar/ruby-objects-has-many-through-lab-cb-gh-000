c3b39b7fff8809dfe2a402632177a2d00a6d5680
Kevin K <doilerjar@outlook.com>
2018-08-12 05:16:37 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,5 +2,16 @@ class Patient
   
   @@all = []
   
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    
+    @@all << self
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
   
 end 
